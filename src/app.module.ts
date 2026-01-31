85a152f0b70af420beb3fd58ddf2da4ca8b575a6
coder-unani <coder.unani@gmail.com>
2026-01-31 22:48:09 +0900

    [feat] 환경변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AdminModule } from './app/admin/admin.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiModule } from './app/api/api.module';
 
 @Module({
-  imports: [AdminModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV
+        ? [`.env.${process.env.NODE_ENV}`, '.env']
+        : ['.env'],
+    }),
+    AdminModule,
+    ApiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
