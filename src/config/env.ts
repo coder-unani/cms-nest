5655fa147b9b5e317be685dde2639b99bc40eb5d
coder-unani <coder.unani@gmail.com>
2026-02-03 00:22:30 +0900

    [feat] 환경변수 설정 파일 추가

---
+++ src/config/env.ts
@@ -0,0 +1,20 @@
+import { registerAs } from '@nestjs/config';
+
+export type DbType =
+  | 'mysql'
+  | 'postgres'
+  | 'sqlite'
+  | 'mariadb'
+  | 'mssql'
+  | 'oracle'
+  | 'mongodb';
+
+export default registerAs('app', () => ({
+  servicePort: process.env.SERVICE_PORT ? Number(process.env.SERVICE_PORT) : 3000,
+  dbType: (process.env.DB_TYPE as DbType) ?? 'mysql',
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+  dbUserName: process.env.DB_USERNAME,
+  dbPassword: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
+}));
